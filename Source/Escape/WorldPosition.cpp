9abab7a1a5cedada0d8a36dcf9d3f8f9d2bb78be
Muhammet Ali <muhammetwas@gmail.com>
2021-11-28 22:10:26 +0300

    created new component

---
+++ Source/Escape/WorldPosition.cpp
@@ -0,0 +1,38 @@
+// Copyright xa-m 2021
+
+
+#include "WorldPosition.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPosition = GetOwner()->GetActorTransform().GetLocation().ToString();
+
+
+	UE_LOG(LogTemp, Warning, TEXT("%s object's position: %s"), *ObjectName, *ObjectPosition);
+
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
